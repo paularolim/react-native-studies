599fb69c11231b2d3150170e6a3981980cc6da24
Paula Rolim <rolim.paula1999@gmail.com>
2022-12-08 10:03:52 -0300

    feat: init modalize playground project

---
+++ ModalizePlayground/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export const App = () => (
+  <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <Text>App.tsx</Text>
+  </View>
+);
